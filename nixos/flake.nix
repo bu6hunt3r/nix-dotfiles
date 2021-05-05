f10023613961fa7200e52e09c497c90b8fa8c7a6
bu6hunt3r <bu6hunt3r@web.de>
2021-05-05 18:14:36 +0200

    Added NUR repository

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,10 +1,14 @@
 {
   # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-20.09";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: {
+  inputs.nur.url = github:nix-community/NUR;
+  outputs = { self, nixpkgs, nur }: {
     nixosConfigurations.nixos-desktop = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        { nixpkgs.overlays = [ nur.overlay ]; }
+      ];
     };
   };
 }
